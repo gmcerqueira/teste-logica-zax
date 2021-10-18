4dae8145cb084b2859b96c0cebcf154444698a9d
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-10-18 14:13:28 -0300

    adicionando pedidos ao entregador

--- teste.js
+++ teste.js
@@ -9,50 +9,46 @@ const lojas = [
 
   { name: "Loja 3", pedidos: [50, 50, 100], taxa: 0.15 },
 ];
-const motos = [
-  {
-    moto1: {
-      nome: "Moto 1",
-      taxa: 2.0,
-      pedidos: [],
-    },
+const motos = {
+  moto1: {
+    nome: "Moto 1",
+    taxa: 2.0,
+    pedidos: [],
   },
-  {
-    moto2: {
-      nome: "Moto 2",
-      taxa: 2.0,
-      pedidos: [],
-    },
-  },
-  {
-    moto3: {
-      nome: "Moto 3",
-      taxa: 2.0,
-      pedidos: [],
-    },
-  },
-  {
-    moto4: {
-      nome: "Moto 4",
-      taxa: 2.0,
-      pedidos: [],
-    },
-  },
-  {
-    moto5: {
-      nome: "Moto 5",
-      taxa: 3.0,
-    },
-  },
-];
-const calculoEntrega = (moto = motos.moto4) => {
-  const loja = lojas.find(({pedidos}) => pedidos.length > 0);
-  const {pedidos} = loja
-  
-  pedidos.forEach(element => {
-  
-});
 
+  moto2: {
+    nome: "Moto 2",
+    taxa: 2.0,
+    pedidos: [],
+  },
+
+  moto3: {
+    nome: "Moto 3",
+    taxa: 2.0,
+    pedidos: [],
+  },
+
+  moto4: {
+    nome: "Moto 4",
+    taxa: 2.0,
+    pedidos: [],
+  },
+
+  moto5: {
+    nome: "Moto 5",
+    taxa: 3.0,
+    pedidos: [],
+  },
+};
+const calculoEntrega = (moto = motos.moto4) => {
+  const loja = lojas.find(({ pedidos }) => pedidos.length > 0);
+  const { pedidos } = loja;
+
+  pedidos.forEach((pedido) => {
+    moto.pedidos.push(pedido);
+  });
+
+  console.log(moto.pedidos);
 };
 
-calculoEntrega();
+calculoEntrega(motos.moto4);
